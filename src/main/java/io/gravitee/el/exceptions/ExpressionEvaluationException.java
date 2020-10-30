1ec2244447c170044df222627c5be486ed67a9d6
Nicolas Géraud <nicolas.geraud@gmail.com>
2020-10-30 17:24:20 +0100

    fix(exception): keep the default constructor
    fix gravitee-io/issues#4514

--- src/main/java/io/gravitee/el/exceptions/ExpressionEvaluationException.java
+++ src/main/java/io/gravitee/el/exceptions/ExpressionEvaluationException.java
@@ -21,6 +21,10 @@ package io.gravitee.el.exceptions;
  */
 public class ExpressionEvaluationException extends RuntimeException {
 
+    public ExpressionEvaluationException(String expression) {
+        super("The template evaluation returns an error. Expression:\n" + expression);
+    }
+
     public ExpressionEvaluationException(String expression, Throwable cause) {
         super("The template evaluation returns an error. Expression:\n" + expression, cause);
     }
