3c3d8624f7d9a0369c6b3e4fddb897c016f59bff
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2024-11-29 14:40:22 +0100

    feat: enable EL resolution for secrets

---
+++ src/main/java/io/gravitee/el/AbstractSpringFactoriesLoaderTemplateVariableProviderFactory.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.el;
+
+import io.gravitee.el.annotations.TemplateVariable;
+import java.util.Arrays;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.support.SpringFactoriesLoader;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public abstract class AbstractSpringFactoriesLoaderTemplateVariableProviderFactory implements TemplateVariableProviderFactory {
+
+    private final Logger logger = LoggerFactory.getLogger(AbstractSpringFactoriesLoaderTemplateVariableProviderFactory.class);
+
+    private List<TemplateVariableProvider> providers;
+
+    @Override
+    public List<TemplateVariableProvider> getTemplateVariableProviders() {
+        if (providers == null) {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            providers =
+                SpringFactoriesLoader
+                    .forDefaultResourceLocation(classLoader)
+                    .load(
+                        TemplateVariableProvider.class,
+                        null,
+                        (cls, factory, err) ->
+                            logger.warn("TemplateVariableProvider loading error for factory {}: {}", factory, err.getMessage())
+                    )
+                    .stream()
+                    .filter(provider -> {
+                        if (provider != null) {
+                            TemplateVariable annotation = provider.getClass().getAnnotation(TemplateVariable.class);
+                            return annotation != null && Arrays.asList(annotation.scopes()).contains(getTemplateVariableScope());
+                        }
+                        return false;
+                    })
+                    .toList();
+        }
+
+        return providers;
+    }
+}
---
+++ src/test/java/io/gravitee/el/spel/TemplateVariableFactoryTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.el.spel;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.gravitee.el.AbstractSpringFactoriesLoaderTemplateVariableProviderFactory;
+import io.gravitee.el.TemplateContext;
+import io.gravitee.el.TemplateVariableProvider;
+import io.gravitee.el.TemplateVariableScope;
+import io.gravitee.el.annotations.TemplateVariable;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.support.SpringFactoriesLoader;
+
+/**
+ * @author Benoit BORDIGONI (benoit.bordigoni at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+class TemplateVariableFactoryTest {
+
+    AbstractSpringFactoriesLoaderTemplateVariableProviderFactory underTest = new TestFactory();
+
+    @Test
+    void should_find_only_one_provider() {
+        assertThat(SpringFactoriesLoader.forDefaultResourceLocation().load(TemplateVariableProvider.class)).hasSize(3);
+        assertThat(underTest.getTemplateVariableProviders()).hasSize(1);
+        assertThat(underTest.getTemplateVariableProviders().get(0)).isInstanceOf(APITemplateVariableProvider.class);
+    }
+
+    static class TestFactory extends AbstractSpringFactoriesLoaderTemplateVariableProviderFactory {
+
+        @Override
+        public TemplateVariableScope getTemplateVariableScope() {
+            return TemplateVariableScope.API;
+        }
+    }
+
+    @TemplateVariable(scopes = TemplateVariableScope.API)
+    public static class APITemplateVariableProvider implements TemplateVariableProvider {
+
+        @Override
+        public void provide(TemplateContext templateContext) {
+            // just testing the class
+        }
+    }
+
+    static class UnAnnotatedTestTemplateProvider implements TemplateVariableProvider {
+
+        @Override
+        public void provide(TemplateContext templateContext) {
+            // just testing the class
+        }
+    }
+
+    @TemplateVariable(scopes = TemplateVariableScope.HEALTH_CHECK)
+    static class WrongScopeTemplateVariableProvider implements TemplateVariableProvider {
+
+        @Override
+        public void provide(TemplateContext templateContext) {
+            // just testing the class
+        }
+    }
+}
